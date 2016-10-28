164961cd4099fa269d1ad252fd1e49c1e0461412
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-10-28 18:54:40 +0200

    feat: navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    public currentTab: number = 0;
+
 }
