7c514913494025fe593ea310d58d4e1076141672
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-11-05 11:28:00 +0100

    fix: correspondeces component works with spaces

--- src/app/correspondences/correspondences.module.ts
+++ src/app/correspondences/correspondences.module.ts
@@ -1,17 +1,19 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {CorrespondencesComponent} from "./correspondences.component";
+import {UtilPipesModule} from "../util-pipes/util-pipes.module";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        UtilPipesModule,
     ],
     declarations: [
         CorrespondencesComponent,
     ],
     exports: [
         CorrespondencesComponent,
-    ]
+    ],
 })
 export class CorrespondencesModule {
 }
